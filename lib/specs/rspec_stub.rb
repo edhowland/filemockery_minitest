bb2c7f76ed43df2d50a08d1dfe4b29a29bb87e28
edhowland <ed.howland@gmail.com>
2015-05-28 21:28:22 +0000

    \'fixed all expectations\'

--- lib/specs/rspec_stub.rb
+++ lib/specs/rspec_stub.rb
@@ -6,7 +6,6 @@ require '../../code/unmockable'
 RSpec.describe File do
   context 'with stub' do
     before(:each) do
-      FileUtils.rm_f './stub'
         allow(File).to receive(:open).and_return(true)
     end
     it 'should stub' do
@@ -15,13 +14,14 @@ RSpec.describe File do
       end
       expect(File).not_to exist('stub')
     end
+  end
 
   context 'with no stub' do
     it 'should write file' do
       File.open('nostub', 'w') do |f|
         f.puts 'hi'
       end
+      expect(File).to exist('nostub')
     end
-  end
   end
 end
