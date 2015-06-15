ef124eb5a3dbbf4042461d0e6ef5fa4e9e64ac2c
edhowland <ed.howland@gmail.com>
2015-06-15 21:46:33 +0000

    \'added threads wait.all_waits to execute.rb\'

--- lib/execute/execute.rb
+++ lib/execute/execute.rb
@@ -1,23 +1,25 @@
 # execute.rb - execute a process returning an array [stdout, stderr, exit_code]
 
 require 'open3'
+require 'thwait'
 
 def execute(command)
   outs = ''
   errs = ''
   status = 0
-Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
+  stdin, stdout, stderr, wait_thr =  Open3.popen3(command) 
+  stdin.close
     outs = stdout.read
     errs = stderr.read
+  ThreadsWait.all_waits wait_thr
     status = wait_thr.status
     [outs, errs, status]
-end
 end
 
 
 puts 'starting date command'
-output = execute('date')
+output = execute(ARGV.first)
 puts 'after date'
 puts "stdout: |#{output[0]}|"
 puts "stderr: |#{output[1]}|"
-puts "exit: |#{output[2]}|"
+puts "exit: |#{output[2].class.name}|"
