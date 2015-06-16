5b1ef20a42a50c07d7ad5b6392817fde50513552
edhowland <ed.howland@gmail.com>
2015-06-16 00:09:30 +0000

    \'fixed exit status\'

--- lib/execute/execute.rb
+++ lib/execute/execute.rb
@@ -4,22 +4,18 @@ require 'open3'
 require 'thwait'
 
 def execute(command)
-  outs = ''
-  errs = ''
-  status = 0
   stdin, stdout, stderr, wait_thr =  Open3.popen3(command) 
   stdin.close
     outs = stdout.read
     errs = stderr.read
-  ThreadsWait.all_waits wait_thr
-    status = wait_thr.status
+  #ThreadsWait.all_waits wait_thr
+  status = wait_thr.value.exitstatus
     [outs, errs, status]
 end
 
-
-puts 'starting date command'
+puts "starting #{ARGV.first} command"
 output = execute(ARGV.first)
-puts 'after date'
+puts 'after'
 puts "stdout: |#{output[0]}|"
 puts "stderr: |#{output[1]}|"
-puts "exit: |#{output[2].class.name}|"
+puts "exit: |#{output[2]}|"
