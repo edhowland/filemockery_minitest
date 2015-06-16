2612fcbdd7fd2c6b87c993e01776a4105e74be1f
edhowland <ed.howland@gmail.com>
2015-06-16 00:25:57 +0000

    \'properly handling exceptions from commands\'

--- lib/execute/execute.rb
+++ lib/execute/execute.rb
@@ -1,16 +1,21 @@
 # execute.rb - execute a process returning an array [stdout, stderr, exit_code]
 
 require 'open3'
-require 'thwait'
 
 def execute(command)
+  begin
+  outs = ''
+    errs = ''
   stdin, stdout, stderr, wait_thr =  Open3.popen3(command) 
   stdin.close
     outs = stdout.read
     errs = stderr.read
-  #ThreadsWait.all_waits wait_thr
-  status = wait_thr.value.exitstatus
-    [outs, errs, status]
+    status = wait_thr.value.exitstatus
+  rescue => err
+    errs = errs + '|' + err.message
+    status = 1
+  end
+  [outs, errs, status]
 end
 
 puts "starting #{ARGV.first} command"
